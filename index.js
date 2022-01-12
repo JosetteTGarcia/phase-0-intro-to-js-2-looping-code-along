30a4d01cffa8a84331690b1bd8fb5d4ce29fa38c
Josette Garcia <josettetgarcia@gmail.com>
2022-01-12 17:58:11 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+let card = [];
+
+function writeCards(names, eventName){
+    for (let i = 0; i < names.length; i++){
+        card.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return card;
+}
+
+
+function countDown(){
+    let startingNum = 10;
+    while (startingNum >= 0) {
+        console.log(startingNum--);
+    }
+}
+
+countDown(10);
